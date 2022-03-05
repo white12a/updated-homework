eab5771f3b697ff4029d253f69ef20226ca13eb7
white12a <bhavesh3583@yahoo.co.in>
2022-03-05 17:27:04 +0000

    updated

---
+++ src/helloworld/PrintNumber.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        String name = "Bhavesh";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(30);
+        System.out.println(name);
+        System.out.println(a + b);
+
+    }
+}
